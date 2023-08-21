36c0246740f3f0cc065e766be3b32ebaf44d20b5
jk-isFullstackDeveloper <jk770225@gmail.com>
2023-08-21 18:27:57 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import ConnectPage from "./Cmp/ConnectPage";
-import BlobPage from "./Cmp/BlobPage";
-import CompanyPage from "./Cmp/CompanyPage";
-import Story from "./Cmp/StoryPage";
-import NewsPage from "./Cmp/NewsPage";
 
 const App = () => {
   return (
     <>
-      <Story />
-      <CompanyPage />
-      <NewsPage />
-      <BlobPage />
-      <ConnectPage />
+      <h1>jitendra kumar</h1>
     </>
   );
 };
