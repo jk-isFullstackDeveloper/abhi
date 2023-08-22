aff100494c3241b09dabaa58bb893f3e0828e467
jk-isFullstackDeveloper <jk770225@gmail.com>
2023-08-22 14:02:50 +0530

    axiso integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Tree from "react-d3-tree";
-
+import axios from "axios";
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
 const orgChart = {
@@ -51,6 +51,22 @@ const orgChart = {
 };
 
 export default function OrgChartTree() {
+  const input = {
+    name: "jitendra",
+    email: "jitendra.ai.dev@gmail.com",
+    mobile: "8340747194",
+  };
+  const url = "http://kamal.feedocenter.com/api/";
+
+  const Get = async () => {
+    const { data } = await axios.post(url, input);
+    console.log(data);
+  };
+
+  useMemo(() => {
+    Get();
+  }, []);
+
   return (
     <div id="treeWrapper" style={{ width: "100%", height: "100vh" }}>
       <Tree orientation={"vertical"} data={orgChart} />
