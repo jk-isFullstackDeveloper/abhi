aedd69a117784dd41bd18f360d8eaa0d7995b0e8
jk-isFullstackDeveloper <jk770225@gmail.com>
2023-08-22 14:25:03 +0530

    axiso-updated and removed warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import Tree from "react-d3-tree";
 import axios from "axios";
 // This is a simplified example of an org chart with a depth of 2.
@@ -51,24 +51,37 @@ const orgChart = {
 };
 
 export default function OrgChartTree() {
-  const input = {
-    name: "jitendra",
-    email: "jitendra.ai.dev@gmail.com",
-    mobile: "8340747194",
-  };
+  const [user, setUser] = useState([]);
+
   const url = "http://kamal.feedocenter.com/api/";
+  useEffect(() => {
+    const input = {
+      name: "jitendra",
+      email: "jitendra.ai.dev@gmail.com",
+      mobile: "8340747194",
+    };
+    const Get = async () => {
+      const { data } = await axios.post(url, input);
 
-  const Get = async () => {
-    const { data } = await axios.post(url, input);
-    console.log(data);
-  };
-
-  useMemo(() => {
+      setUser([data.data]);
+    };
     Get();
   }, []);
 
   return (
     <div id="treeWrapper" style={{ width: "100%", height: "100vh" }}>
+      <div style={{ width: "100%", height: "30px", padding: "10px" }}></div>
+      <div>
+        {!user && <small>Loading...</small>}
+        {user &&
+          user.map((item) => (
+            <ul>
+              <li>{item.name}</li>
+              <li>{item.email}</li>
+              <li>{item.mobile}</li>
+            </ul>
+          ))}
+      </div>
       <Tree orientation={"vertical"} data={orgChart} />
     </div>
   );
